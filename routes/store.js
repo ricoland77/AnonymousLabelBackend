8435937b6f2cbc8c17f6c8f4a454611aea1d54a1
mac-ricoland <ricoland@MacBook-Pro.local>
2023-09-11 13:09:19 +0200

    remove  log

--- routes/store.js
+++ routes/store.js
@@ -17,7 +17,6 @@ const Store = require("../models/Store");
 router.get(`/store/projects/`, async (req, res) => {
   try {
     res.status(200).json(store.projects);
-    console.log(store.projects);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
